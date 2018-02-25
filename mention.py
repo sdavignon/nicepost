e6d3f3f291de5e3b56bd190bb50446a9ab0b2e3d
Seungmin Kim <8457324+smkm@users.noreply.github.com>
2018-02-25 21:26:11 +0900

    Update mention.py

--- mention.py
+++ mention.py
@@ -5,3 +5,7 @@ from steem import Steem
 from steem.blockchain import Blockchain
 from steem.post import Post
 
+upvotePercent = 1
+minRep = 30
+accountName = 'exampleaccount'
+postingKey = '' # I heard it is possible to set this in the terminal and get that posting key, possibly more secure. 
